b4d04ca5f8bf4cfde9b5ab3629cdcc2ee8b84e8d
sairam2381 <sairam2381@gmail.com>
2025-04-05 12:48:49 +0530

    The recent code

--- controllers/detailsFunction.js
+++ controllers/detailsFunction.js
@@ -4,12 +4,12 @@ exports.fetchUserDetails = async (req, res) => {
     const getDetails = await User.find({});
     return res.status(200).json({
       success: true,
-      data: getDetails,
+      data123: getDetails,
     });
   } catch (e) {
     return res.status(404).json({
       success: false,
-      message: e,
+      //   message: e,
     });
   }
 };
--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const app = express();
 const userDetails = require("./routes/details");
 const database = require("./config/database");
 dotenv.config();
 database.connect();
-app.use("/api/v1/user", userDetails);
+
 app.get("/sairam", (req, res) => {
   return res.json({
     success: false,
     message: "Welcome to the server",
   });
 });
+
 const port = process.env.port;
+app.use(
+  cors({
+    origin: ["*", "http://localhost:3000"],
+    credentials: true,
+  })
+);
+app.use("/api/v1/user", userDetails);
+
 console.log("PORT NUMBER:", port);
 app.listen(port, async () => {
   console.log("Server is running on port 25000");
